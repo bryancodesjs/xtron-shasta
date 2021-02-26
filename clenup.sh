f0a1e2a908174c6059877f1ec634317b31abf3be
gestrella0714 <georgie.estrella@gmail.com>
2021-02-25 22:05:45 -0400

    Adding updates

---
+++ clenup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -fr asset-manifest.json
+rm -fr favicon.ico
+rm -fr fonts
+rm -fr index.html
+rm -fr js
+rm -fr logo*.png
+rm -fr manifest.json
+rm -fr robots.txt
+rm -fr static
+rm -fr web.config
